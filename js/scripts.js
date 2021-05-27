148015c5c2e31fec7e5a91be23787bc3ef454829
Timothy Lung <timothylung1996@gmail.com>
2021-05-26 23:22:19 -0500

    message us form

--- js/scripts.js
+++ js/scripts.js
@@ -114,31 +114,6 @@ $(document).ready(function () {
 
     });
 
-    /********************** Social Share buttons ***********************/
-    var share_bar = document.getElementsByClassName('share-bar');
-    var po = document.createElement('script');
-    po.type = 'text/javascript';
-    po.async = true;
-    po.src = 'https://apis.google.com/js/platform.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
-
-    for (var i = 0; i < share_bar.length; i++) {
-        var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
-            'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
-            'style="width:105px; height:21px;">' +
-            '</iframe>' +
-
-            '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=101094500229731&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
-
-            '<div class="g-plusone" data-size="medium"></div>';
-
-        // '<iframe src="https://plusone.google.com/_/+1/fastbutton?bsv&amp;size=medium&amp;url=' + encodeURIComponent(window.location) + '" allowtransparency="true" frameborder="0" scrolling="no" title="+1" style="width:105px; height:21px;"></iframe>';
-
-        share_bar[i].innerHTML = html;
-        share_bar[i].style.display = 'inline-block';
-    }
-
     /********************** Embed youtube video *********************/
     $('.player').YTPlayer();
 
@@ -189,31 +164,21 @@ $(document).ready(function () {
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
-        console.log('hello');
-        console.log(MD5($('#invite_code').val()));
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
-
-        if (MD5($('#invite_code').val()) !== 'f70587e49c294a71345dbde75faa6f77') {
-              console.log('what on earth');
-              console.log(MD5($('#invite_code').val()));
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-          console.log('where is this message going');
-            $.post('https://script.google.com/macros/s/AKfycbyT5NYzRfDE60C_91ABRmuEQt4qzrQUJkiQ1MOZhy402kFJn5me/exec', data)
-                .done(function (data) {
-                    console.log(data);
-                    if (data.result === "error") {
-                        $('#alert-wrapper').html(alert_markup('danger', data.message));
-                    } else {
-                        $('#alert-wrapper').html('');
-                        $('#rsvp-modal').modal('show');
-                    }
-                })
-                .fail(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                });
-        }
+          $.post('https://script.google.com/macros/s/AKfycbyMMH3Ro8vPo2Tjk5suSpyJJvNR-AlnGCq7RQ5lqA/exec', data)
+              .done(function (data) {
+                  console.log(data);
+                  if (data.result === "error") {
+                      $('#alert-wrapper').html(alert_markup('danger', data.message));
+                  } else{
+                      $('#alert-wrapper').html(alert_markup('info', '<strong>Sent! Thank you!</strong>'));
+                      $('#rsvp-modal').modal('show');
+                  }
+              })
+              .fail(function (data) {
+                  console.log(data);
+                  $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+              });
     });
 
 });
